b5dfff6eb9c8cb225a91826f2854517a424a0634
MLundberg82 <mattiaslundberg82@gmail.com>
2026-09-01 18:12:53 +0200

    Deduplicate level one animation frames

--- lib/core/assets/gymrat_assets.dart
+++ lib/core/assets/gymrat_assets.dart
@@ -26,24 +26,24 @@ abstract final class GymRatAssets {
     'assets/characters/male/level_01/idle/idle_06.png',
     'assets/characters/male/level_01/idle/idle_07.png',
     'assets/characters/male/level_01/idle/idle_08.png',
-    'assets/characters/male/level_01/idle/idle_09.png',
-    'assets/characters/male/level_01/idle/idle_10.png',
-    'assets/characters/male/level_01/idle/idle_11.png',
-    'assets/characters/male/level_01/idle/idle_12.png',
-    'assets/characters/male/level_01/idle/idle_13.png',
-    'assets/characters/male/level_01/idle/idle_14.png',
+    'assets/characters/male/level_01/idle/idle_07.png',
+    'assets/characters/male/level_01/idle/idle_06.png',
+    'assets/characters/male/level_01/idle/idle_05.png',
+    'assets/characters/male/level_01/idle/idle_04.png',
+    'assets/characters/male/level_01/idle/idle_03.png',
+    'assets/characters/male/level_01/idle/idle_02.png',
     'assets/characters/male/level_01/idle/idle_15.png',
   ];
 
   static const List<String> maleLevel1BlinkFrames = [
-    'assets/characters/male/level_01/blink/blink_00.png',
+    'assets/characters/male/level_01/idle/idle_00.png',
     'assets/characters/male/level_01/blink/blink_01.png',
     'assets/characters/male/level_01/blink/blink_02.png',
     'assets/characters/male/level_01/blink/blink_03.png',
   ];
 
   static const List<String> maleLevel1TailFrames = [
-    'assets/characters/male/level_01/tail/tail_00.png',
+    'assets/characters/male/level_01/idle/idle_00.png',
     'assets/characters/male/level_01/tail/tail_01.png',
     'assets/characters/male/level_01/tail/tail_02.png',
     'assets/characters/male/level_01/tail/tail_03.png',
